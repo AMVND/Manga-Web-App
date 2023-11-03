d9df82f0fd6cae3ecc86cec94265e3c0a9542ca8
AMV_ND <anivnmusicvideo@gmail.com>
2023-11-03 10:46:55 +0700

    remove_api_navigation

--- front_client/src/api/Navigation.js
+++
@@ -1,6 +0,0 @@
-export const navigation = [
-    { name: 'Home', href: '/home', current: true },
-    { name: 'Admin', href: '/admin', current: false },
-    { name: 'Mod', href: '/mod', current: false },
-    { name: 'User', href: '/user', current: false },
-]
